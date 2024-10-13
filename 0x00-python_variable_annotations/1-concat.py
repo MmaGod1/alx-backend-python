855cb4b33f9b781f084f8a7a24b0323eeccd3308
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-13 16:07:17 +0100

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to concatenate two strings.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenation of str1 and str2.
+    """
+    return str1 + str2
