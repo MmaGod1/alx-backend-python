3e376c9b8763b5140be71fa448aed627ce3fc889
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-13 16:14:54 +0100

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to convert a float to its string representation.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of n.
+    """
+    return str(n)
