c723ebb30d1ef6e9c189657bcfce308844fefce9
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-13 17:00:48 +0100

    Pycodestyle fix

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-"""
-Module to zoom in on an array based on a specified factor.
-"""
+"""Module to zoom in on an array based on a specified factor."""
 from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
-    """
-    Returns a new list with each element of the input tuple repeated
-    based on the zoom factor.
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """Creates multiple copies of items in a tuple.
+    
+    Args:
+        lst (Tuple[int, ...]): A tuple of integers.
+        factor (int): The number of times to repeat each item.
+
+    Returns:
+        List[int]: A list containing the zoomed-in values.
     """
     zoomed_in: List[int] = [
         item for item in lst
@@ -19,5 +22,4 @@ def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
