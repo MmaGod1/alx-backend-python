7b93b513c99aa8b3cd8654288ae0b2b060b9b22d
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-14 20:45:16 +0100

    Create 4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to execute multiple asyncio tasks.
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Executes task_wait_random n times.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    wait_times = await asyncio.gather(*tasks)
+    return sorted(wait_times)
