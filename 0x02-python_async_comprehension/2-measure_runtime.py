3e9b728e4700f53d9fb7f261bd032addf9b865a0
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-16 12:09:35 +0100

    Line too long

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = load_module('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measures the runtime of running async_comprehension four times in parallel.
+    """Measures the runtime of running async_comprehension.
     """
     start = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
